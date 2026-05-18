b6faccf1bc4378bbe05bb2e0fc60c65a98c8e805
Keerthi-DSK <keerthidhayal9@gmail.com>
2026-05-18 17:27:06 +0530

    Add files via upload
    Week1-Home Assignments

---
+++ week1/gradeCalculation.js
@@ -0,0 +1,33 @@
+function getGrade(score) {
+  let grade; 
+  
+  switch (true) {
+    case (score >= 90 && score <= 100):
+      grade = "A";
+      break;
+
+    case (score >= 80 && score < 90):
+      grade = "B";
+      break;
+
+    case (score >= 70 && score < 80):
+      grade = "C";
+      break;
+
+    case (score >= 60 && score < 70):
+      grade = "D";
+      break;
+
+    case (score >= 0 && score < 60):
+      grade = "F";
+      break;
+
+    default:
+      grade = "Invalid score";
+      break;
+  }
+  return grade;
+}
+             
+console.log(`Grade of the Student: ${getGrade(73)}`);
+
---
+++ week1/learnDatatypes.js
@@ -0,0 +1,11 @@
+const browserVersion="Chrome";
+function getBrowserVersion()
+{
+if(browserVersion=="Chrome")
+{
+    let browserVersion="Safari";
+    //console.log(browserVersion);
+}
+console.log(browserVersion);
+}
+getBrowserVersion();
