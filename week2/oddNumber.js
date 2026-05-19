744da6f8927532fdf55cc70eb0fa35f46485caa7
Keerthi-DSK <keerthidhayal9@gmail.com>
2026-05-19 12:33:44 +0530

    Add files via upload

---
+++ week2/oddNumber.js
@@ -0,0 +1,8 @@
+let n=30;
+for(let i=0; i<=n; i++)
+{
+    if(i%2==1)
+    {
+     console.log(i);
+    }
+}
---
+++ week2/week2day1.js
@@ -0,0 +1,7 @@
+let name="Salai Keerthi";
+let n=name.split("");
+for(let i=(n.length); i>=0; i--)
+{
+  console.log(n[i]);
+
+}
